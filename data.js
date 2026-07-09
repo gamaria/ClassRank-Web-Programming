68fb5c1ad0ce9869c310ea81b2ee8f1196154578
gamar <gamariamandar20@gmail.com>
2026-07-09 11:00:48 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+// PUSAT DATA UTAMA (Edit nilai di sini jika ada perubahan)
+const dataKelompok = [
+    { nama: "Muadz Jahir, Ridho Ariyanto, Rizki Ditiratu, Ewis Jola /INFO-1", laporan: 95, website: 90, presentasi: 88, kekompakan: 90 },
+    { nama: "Zelda, Intan, M Zulkarnain, Febriansya / INFO-1", laporan: 85, website: 95, presentasi: 81, kekompakan: 82 },
+    { nama: "Afrizal Nurman, Mazwa, M Fauzan, Farid/ INFO-1", laporan: 80, website: 80, presentasi: 60, kekompakan: 61 },
+    { nama: "Mayang, Febrian, Eko Prasetya, Nulania/ INFO-1", laporan: 80, website: 75, presentasi: 60, kekompakan: 50 },
+    { nama: "Irfan Ikbal, Selfi Kaliadi, Fadhil, Mukadim/ INFO-1", laporan: 90, website: 81, presentasi: 80, kekompakan: 81 },
+    { nama: "Adnan / INFO-1", laporan: 70, website: 90, presentasi: 80, kekompakan: 0 },
+    { nama: "Ibnu Khaldun, Algea, Yusmad, Rey / INFO-3", laporan: 65, website: 80, presentasi: 67, kekompakan: 70 },
+];
+
+const dataIndividu = [
+    { nama: "Muadz", kelompok: "Info 1", presentasi: 78, database: 50, kode: 50 },
+    { nama: "Ridho Ariyanto", kelompok: "Info 1", presentasi: 80, database: 75, kode: 82 },
+    { nama: "Rizki Diratu", kelompok: "Info 1", presentasi: 85, database: 65, kode: 60 },
+    { nama: "Ewis Jola", kelompok: "Info 1", presentasi: 80, database: 65, kode: 60 },
+    { nama: "Zelda", kelompok: "Info 1", presentasi: 80, database: 65, kode: 65 },
+    { nama: "Intan", kelompok: "Info 1", presentasi: 70, database: 60, kode: 59 },
+    { nama: "M Zulkarnain", kelompok: "Info 1", presentasi: 80, database: 75, kode: 78 },
+    { nama: "Febriansyah", kelompok: "Info 1", presentasi: 60, database: 50, kode: 50 },
+    { nama: "Afrizal", kelompok: "Info 1", presentasi: 80, database: 70, kode: 75 },
+    { nama: "Mazwa Masud", kelompok: "Info 1", presentasi: 60, database: 50, kode: 50 },
+    { nama: "M Fauzan", kelompok: "Info 1", presentasi: 69, database: 56, kode: 56 },
+    { nama: "Farid Perbela", kelompok: "Info 1", presentasi: 51, database: 45, kode: 45 },
+    { nama: "Mayang", kelompok: "Info 1", presentasi: 70, database: 60, kode: 60 },
+    { nama: "Febrian M Nur", kelompok: "Info 1", presentasi: 50, database: 45, kode: 45 },
+    { nama: "Eko Prasetya M", kelompok: "Info 1", presentasi: 80, database: 75, kode: 80 },
+    { nama: "Nurlania", kelompok: "Info 1", presentasi: 50, database: 45, kode: 45 },
+    { nama: "Irfan Ikbal", kelompok: "Info 1", presentasi: 80, database: 75, kode: 70 },
+    { nama: "Selfi Kaliadi", kelompok: "Info 1", presentasi: 81, database: 70, kode: 75 },
+    { nama: "Fadhil", kelompok: "Info 1", presentasi: 70, database: 60, kode: 60 },
+    { nama: "Mukadim Rizal", kelompok: "Info 1", presentasi: 49, database: 45, kode: 45 },
+    { nama: "Ibnu Khaldun", kelompok: "Info 3", presentasi: 70, database: 45, kode: 45 },
+    { nama: "Algea Faris", kelompok: "Info 1", presentasi: 70, database: 65, kode: 60 },
+    { nama: "Yusmad", kelompok: "Info 1", presentasi: 60, database: 45, kode: 45 },
+    { nama: "Rey", kelompok: "Info 1", presentasi: 50, database: 45, kode: 45 },
+
+
+
+
+    
+
+
+];
