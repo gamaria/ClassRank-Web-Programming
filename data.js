1a30b04f7d5fef9277ec82c5f7acc1f15293d578
gamar <gamariamandar20@gmail.com>
2026-07-11 11:35:15 +0900

    Update data.js

--- data.js
+++ data.js
@@ -6,6 +6,8 @@ const dataKelompok = [
     { nama: "Mayang, Febrian, Eko Prasetya, Nulania/ INFO-1", laporan: 80, website: 75, presentasi: 60, kekompakan: 50 },
     { nama: "Irfan Ikbal, Selfi Kaliadi, Fadhil, Mukadim/ INFO-1", laporan: 90, website: 81, presentasi: 80, kekompakan: 81 },
     { nama: "Adnan / INFO-1", laporan: 70, website: 90, presentasi: 80, kekompakan: 0 },
+    { nama: "Ariyandi Abdullah, M.Asyik, M.Fajar, Zicry, Abdullah", laporan: 55, website: 70, presentasi: 70, kekompakan: 75 },
+    { nama: "Fatrin, Maya, riris", laporan: 55, website: 79, presentasi: 60, kekompakan: 80 },
     { nama: "Ibnu Khaldun, Algea, Yusmad, Rey / INFO-3", laporan: 65, website: 80, presentasi: 67, kekompakan: 70 },
 ];
 
@@ -30,10 +32,17 @@ const dataIndividu = [
     { nama: "Selfi Kaliadi", kelompok: "Info 1", presentasi: 81, database: 70, kode: 75 },
     { nama: "Fadhil", kelompok: "Info 1", presentasi: 70, database: 60, kode: 60 },
     { nama: "Mukadim Rizal", kelompok: "Info 1", presentasi: 49, database: 45, kode: 45 },
+    { nama: "Fatrin", kelompok: "Info 2", presentasi: 75, database: 50, kode: 50 },
+    { nama: "Maya", kelompok: "Info 2", presentasi: 75, database: 55, kode: 55 },
+    { nama: "Riris", kelompok: "Info 2", presentasi: 75, database: 50, kode: 50 },
+    { nama: "Nazla", kelompok: "Info 2", presentasi: 70, database: 45, kode: 45 },
+    { nama: "Ariyandi", kelompok: "Info 2", presentasi: 75, database: 50, kode: 50 },
+    { nama: "M Asyik", kelompok: "Info 2", presentasi: 70, database: 50, kode: 45 },
+    { nama: "M Fajar", kelompok: "Info 2", presentasi: 75, database: 65, kode: 75 },
     { nama: "Ibnu Khaldun", kelompok: "Info 3", presentasi: 70, database: 45, kode: 45 },
-    { nama: "Algea Faris", kelompok: "Info 1", presentasi: 70, database: 65, kode: 60 },
-    { nama: "Yusmad", kelompok: "Info 1", presentasi: 60, database: 45, kode: 45 },
-    { nama: "Rey", kelompok: "Info 1", presentasi: 50, database: 45, kode: 45 },
+    { nama: "Algea Faris", kelompok: "Info 3", presentasi: 70, database: 65, kode: 60 },
+    { nama: "Yusmad", kelompok: "Info 3", presentasi: 60, database: 45, kode: 45 },
+    { nama: "Rey", kelompok: "Info 3", presentasi: 50, database: 45, kode: 45 },
 
 
 
